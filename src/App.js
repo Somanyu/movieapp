14530bb6904198ed3187f986892e3c52d184e4b0
Somanyu <somanyu.03samal@gmail.com>
2022-12-12 12:17:35 +0530

    💄 update: card responsiveness

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
       </div>
 
       <div className='container my-12 mx-auto px-4 md:px-12'>
-        <div className='flex flex-wrap justify-center -mx-1 lg:-mx-4'>
+        <div className='flex flex-wrap justify-around -mx-1 lg:-mx-4'>
           <Card />
           <Card />
           <Card />
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { FaHeart } from 'react-icons/fa'
+import { FaHeart } from 'react-icons/fa';
 
 const Card = () => {
     return (
-        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <div class="text-left max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex justify-center my-1 px-2 w-full md:w-1/2 lg:my-4 lg:px-9 lg:w-1/4">
+            <div class="text-left w-72 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                 <a href="/">
-                    <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                    <img class="rounded-t-lg" src="https://m.media-amazon.com/images/M/MV5BOTQ5Nzc3NzAtMzZlMS00ZWJjLWIxMGMtNDU4ZTQ1NmNjMjc5XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg" width="280" alt="" />
                 </a>
                 <div class="p-5">
                     <a href="/">
                         <h5 class="font-karla mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
                     </a>
-                    <p class="font-inter mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <p class="font-inter mb-3 font-normal text-gray-700 dark:text-gray-400">Year: 2012</p>
                     <div className='container flex justify-between'>
                         <div>
                             <a href="/" class="font-inter inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
