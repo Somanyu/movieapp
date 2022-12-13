0e5a9f891b43e82d3b7340c6d54da13775e3845b
Somanyu <somanyu.03samal@gmail.com>
2022-12-13 09:21:22 +0530

    🚑 update: add and remove in separate components

---
+++ src/components/RemoveFavourite.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaHeart } from 'react-icons/fa';
+import { FiHeart } from 'react-icons/fi';
+
+
+const RemoveFavourite = () => {
+    return (
+        <FaHeart className="text-red-700 hover:text-red-800 text-2xl mt-1" />
+    );
+}
+
+export default RemoveFavourite;
