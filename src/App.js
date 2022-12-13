0513c38c7690babdd0598d107b9208b8e88fdccd
Somanyu <somanyu.03samal@gmail.com>
2022-12-13 19:40:05 +0530

    💄 update: added speed dial

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import AddFavourite from './components/AddFavourite';
 import RemoveFavourite from './components/RemoveFavourite';
+import { FaHeart } from 'react-icons/fa';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -50,7 +51,7 @@ function App() {
             <Search searchValue={searchValue} setSearchValue={setSearchValue} />
           </div>
           <div class="w-72 h-32 mt-3">
-            <a type="button" href='#favourites' class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 font-inter">Favourites</a>
+            <a type="button" href='#favourites' class=" text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 font-inter">Favourites</a>
           </div>
         </div>
       </div>
@@ -65,7 +66,7 @@ function App() {
         </div>
       </div>
 
-      <h1 id='favourites' class=" mt-8 mb-4 font-karla text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"><mark class="px-2 text-white bg-yellow-400 rounded dark:bg-yellow-400">OMDb</mark> favourite movie</h1>
+      <h1 id='favourites' class="mt-13 pt-10 pb-5 mb-4 font-karla text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"><mark class="px-2 text-white bg-yellow-400 rounded dark:bg-yellow-400">OMDb</mark> favourite movie</h1>
 
       <div className='container my-12 mx-auto px-4 md:px-12'>
         <div className='flex flex-wrap justify-around -mx-1 lg:-mx-4'>
@@ -76,6 +77,20 @@ function App() {
           />
         </div>
       </div>
+
+
+      <div data-dial-init class="flex fixed right-6 bottom-6 group">
+        <div id="speed-dial-menu-horizontal" class="hidden items-center mr-4 space-x-2">
+          Favourites
+        </div>
+        <button type="button" data-dial-toggle="speed-dial-menu-horizontal" aria-controls="speed-dial-menu-horizontal" aria-expanded="false" class="flex justify-center items-center w-14 h-14 text-white bg-red-700 rounded-full hover:bg-red-800 dark:bg-red-600 dark:hover:bg-red-700 focus:ring-4 focus:ring-red-300 focus:outline-none dark:focus:ring-red-800">
+          <a href="#favourites">
+            <FaHeart className='text-2xl' />
+          </a>
+          <span class="sr-only">Open actions menu</span>
+        </button>
+      </div>
+
     </div>
   );
 }
